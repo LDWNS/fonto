1c633b99d32b5038e7f4a29d3148a530b2f59ec4
LDWNS <lukasdownes@gmail.com>
2026-04-20 17:48:51 +0200

    chore(edit-point): cleanup

--- scripts/classes/editpoint.js
+++ scripts/classes/editpoint.js
@@ -21,18 +21,17 @@ export class EditPoint extends SVGCircle {
     const ep = new EditPoint(x, y, key);
     if (x1 != undefined && y1 != undefined) {
       const a1 = new EditPoint(x1, y1, `${key}-a-0`, 3)
-        .setAttribute("fill", "#333")
         .setAnchorPoint(prevEPoint)
         .createAnchorLine();
       eps.push(a1);
     }
     if (x2 != undefined && y2 != undefined) {
       const a2 = new EditPoint(x2, y2, `${key}-a-1`, 3)
-        .setAttribute("fill", "#333")
         .setAnchorPoint(ep)
         .createAnchorLine();
       eps.push(a2);
     }
+    ep.linkPrev(prevEPoint);
     return [ep, ...eps];
   }
 
@@ -56,18 +55,18 @@ export class EditPoint extends SVGCircle {
     return this;
   }
 
-  #anchorPointUpdateStrategy(anchoredPoint, dx, dy) {
-    // anchoredPoint.update({ x: anchoredPoint.x + dx, y: anchoredPoint.y + dy });
-    if (anchoredPoint.anchorLine) {
-      anchoredPoint.anchorLine
-        .setCoords({
-          x1: this.x,
-          y1: this.y,
-          x2: anchoredPoint.x,
-          y2: anchoredPoint.y,
-        })
-        .update({ x: undefined, y: undefined }, () => {});
+  linkPrev(prevEPoint) {
+    if (prevEPoint) {
+      this.prevEP = prevEPoint;
+      prevEPoint.nextEP = this;
     }
+  }
+
+  setAnchorPoint(anchorPoint) {
+    this.setAttribute("fill", "#333");
+    this.anchorPoint = anchorPoint;
+    anchorPoint.addAnchoredPoint(this);
+    return this;
   }
 
   addAnchoredPoint(anchoredPoint) {
@@ -81,10 +80,9 @@ export class EditPoint extends SVGCircle {
     }
   }
 
-  setAnchorPoint(anchorPoint) {
-    this.anchorPoint = anchorPoint;
-    anchorPoint.addAnchoredPoint(this);
-    return this;
+  #anchorPointUpdateStrategy(anchoredPoint, dx, dy) {
+    anchoredPoint
+      .update({ x: anchoredPoint.x + dx, y: anchoredPoint.y + dy });
   }
 
   createAnchorLine() {
